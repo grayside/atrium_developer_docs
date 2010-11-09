9d7bfbaf2929903ec3d1386f40f38d16c0cae88e
Grayside <grayside@gmail.com>
2010-11-09 12:30:11 -0800

    Initial commit.

---
+++ features.php
@@ -0,0 +1,16 @@
+<?php
+// $Id$
+
+/** 
+ * @file
+ *   Working with Features
+ *
+ * Open Atrium is built with a set of functionality provided by the
+ * @link http://drupal.org/project/features Features @endlink module infrastructure,
+ * and as a platform for further Features-based extension. A solid grounding in
+ * features is a very good idea.
+ *
+ * - @link https://community.openatrium.com/documentation-en/node/1943 What Are Features and How Features Help @endlink
+ * - @link https://community.openatrium.com/documentation-en/node/449 Build a New Feature @endlink
+ * - @link https://community.openatrium.com/documentation-en/node/690 Update an Existing Feature @endlink
+ */
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+// $Id$
+
+/** @mainpage Developer Documentation
+ * Welcome to the @link http://openatrium.com Open Atrium @endlink developer's documentation. Newcomers to Open Atrium
+ * should read up on the basics of Drupal development before continuing, as Atrium
+ * development builds off at least an intermediate understanding of how Drupal works.
+ *
+ * This documentation jumps into the plumbing that makes Atrium work.
+ *
+ * - The Open Atrium Stack
+ *   - @link context.module Context @endlink
+ *   - @link features.module Features @endlink
+ *   - @link spaces.module Spaces @endlink
+ *   - @link purl.module PURL @endlink
+ * - Developing & Customizing Atrium
+ *   - @link https://community.openatrium.com/documentation-en/node/1420 Build Open Atrium for Development @endlink
+ *   - @link https://community.openatrium.com/documentation-en/node/1951 Open Atrium Architecture @endlink
+ *   - @link https://community.openatrium.com/documentation-en/node/1944 Customization Guide @endlink
+ *   - @link https://community.openatrium.com/documentation-en/node/590 Customization Examples @endlink
+ * - Site Building in OpenAtrium
+ *   - @link features.php Working with Features @endlink
+ * - Community Discussion
+ *   - @link http://community.openatrium.com Open Atrium Community Site @endlink
+ *   - Channel #open_atrium on irc.freenode.net
+ */
